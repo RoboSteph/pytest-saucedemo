ed7e678e7f5c941ee653e0cac533b56480b4b5cd
unknown <vardalagoy@gmail.com>
2025-07-30 14:50:42 -0700

    Setting up credentials

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,11 @@
 import pytest
 from playwright.sync_api import sync_playwright
+import yaml
+
+def load_credentials():
+    with open("credentials.yaml", "r") as file:
+        credentials = yaml.safe_load(file)
+    return credentials
 
 @pytest.fixture(scope="function")
 def browser():
@@ -11,10 +17,11 @@ def browser():
         browser.close()
 
 def test_login(browser):
+    credentials = load_credentials()
     page = browser
     page.goto("https://www.saucedemo.com/")
-    page.fill('input[data-test="username"]', "standard_user")
-    page.fill('input[data-test="password"]', "secret_sauce")
+    page.fill('input[data-test="username"]', credentials["username"])
+    page.fill('input[data-test="password"]', credentials["password"])
     page.click('input[data-test="login-button"]')
     assert not page.is_visible('h3[data-test="error"]') # check for login error message
     assert page.is_visible('div.inventory_list') # check if list of products is visible
